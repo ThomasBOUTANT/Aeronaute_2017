e24efafe12f60b4d8e0dd2abbcac95e5641b3525
Roland Bavelard <rolan.bavelard@hotmail.fr>
2017-11-18 11:47:30 +0100

    +caméra
    Ajout du déplacement vertical de la caméra dans la scène

--- Montgolfière/Assets/Scripts/CameraPlayer.cs
+++ Montgolfière/Assets/Scripts/CameraPlayer.cs
@@ -2,18 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraPlayer : MonoBehaviour {
+public class CameraPlayer : MonoBehaviour
+{
 
     [SerializeField]
     private GameObject player;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    [SerializeField]
+    public float minZ; //la camera ne va pas plus haut 
+
+    [SerializeField]
+    public float maxZ; // la camera ne va pas plus bas 
+
+
+    // Use this for initialization 
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame 
+    void Update()
+    {
         float newX = player.transform.position.x;
-        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
-	}
+        float newZ = player.transform.position.z;
+
+        if (newZ < minZ || newZ > maxZ)
+        {
+            transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+        }
+        else
+        {
+            transform.position = new Vector3(newX, transform.position.y, newZ);
+        }
+    }
 }
