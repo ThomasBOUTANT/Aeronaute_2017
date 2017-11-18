52282de52baeca6a373aef8c419b88fc400697da
Roland Bavelard <rolan.bavelard@hotmail.fr>
2017-11-18 10:37:20 +0100

    Ajout du déplacement et des dégats

---
+++ Montgolfière/Assets/Scripts/CameraPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPlayer : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject player;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float newX = player.transform.position.x;
+        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+	}
+}
--- Montgolfière/Assets/Scripts/Intemperie.cs
+++ Montgolfière/Assets/Scripts/Intemperie.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public enum WeatherType { Rain,Thunder};
+public enum WeatherType {Wind,Rain,Bruine,Snow,Grele,Myst,Storm,SandStorm,AshStorm};
 
 public class Intemperie : MonoBehaviour {
 
+
     [SerializeField]
-    private float damages;
+    private Damageables typeDamagables;
+    [SerializeField]
+    private float damages,delayDamages;
 
     [SerializeField]
     private WeatherType type;
+
+
+    private float timerDamages;
+
+    [SerializeField]
+    private float baseSpeed;
     //[SerializeField]
     //type ? resourceDamanged;
 
@@ -20,6 +29,7 @@ public class Intemperie : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         appearingDistance = transform.position.x;
+        timerDamages = Time.time;
 	}
 	
 	// Update is called once per frame
@@ -27,14 +37,24 @@ public class Intemperie : MonoBehaviour {
 		
 	}
 
-    void touchPlayer()
+    public void TouchPlayer(PlayerMovement player)
     {
+        if (timerDamages < Time.time)
+        {
+            player.Damages(typeDamagables, damages);
+            timerDamages = Time.time + delayDamages;
+        }
 
     }
 
     public float GetAppearingDistance()
     {
         return appearingDistance;
+    }
+
+    public float GetBaseSpeed()
+    {
+        return baseSpeed;
     }
 
 
